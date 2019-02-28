d8aa60ca26a3ac0d33507ebd24e9408a49b73972
lucasGabrielDeAA <lg.technus@gmail.com>
2019-02-28 14:13:50 -0300

    Persist informations on database, and other functionalities added to application's index

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
+//Initializing app
 const app = express();
 
+//Initializing DB
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi',
+    { useNewUrlParser: true }
+);
+
+//Importing application's models
+requireDir('./src/models/');
+
+const Product = mongoose.model('Product');
+
+//First route
 app.get('/', (req, res) => {
-    res.send('Hello developer');
+    Product.create({
+        title: 'Test',
+        description: 'Product test',
+        url: 'www.google.com',
+    });
+
+    return res.send('Hello developer');
 });
 
 app.listen(3001);
