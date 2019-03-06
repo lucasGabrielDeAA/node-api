5e509aa0063096fd1a89e183087eff4b9489b4ff
lucasGabrielDeAA <lg.technus@gmail.com>
2019-03-06 15:20:38 -0300

    Cors added to API

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+const cors = require('cors');
 
 //Initializing app
 const app = express();
 app.use(express.json());
+
+//Using cross-origin request security
+app.use(cors());
 
 //Initializing DB
 mongoose.connect(
