26d1b45b0ed8e8cd514c6fdbacaabd5d497eddaf
lucasGabrielDeAA <lg.technus@gmail.com>
2019-03-06 15:06:58 -0300

    CRUD's routes added

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,9 @@ const routes = express.Router();
 const ProductController = require('./controllers/ProductController');
 
 routes.get('/products', ProductController.all);
+routes.get('/products/:id', ProductController.getByID);
 routes.post('/products', ProductController.save);
+routes.put('/products/:id', ProductController.update);
+routes.delete('/products/:id', ProductController.remove);
 
 module.exports = routes;
