5471d14011e231097ca13a9c854b6815c21fb798
lucasGabrielDeAA <lg.technus@gmail.com>
2019-03-06 15:06:47 -0300

    CRUD's functionalities added t ProductController

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -9,9 +9,31 @@ module.exports = {
         return res.json(products);
     },
 
+    async getByID(req, res) {
+        const product = await Product.findById(req.params.id);
+
+        return res.json(product);
+    },
+
     async save(req, res) {
         const product = await Product.create(req.body);
 
         return res.json(product);
+    },
+
+    async update(req, res) {
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+
+        return res.json(product);
+    },
+
+    async remove(req, res) {
+        try {
+            await Product.findByIdAndRemove(req.params.id);
+
+            return res.send('Product removed');
+        } catch (error) {
+            return res.send(`Could not remove the product with the specified id: ${req.params.id}`);
+        }
     },
 };
