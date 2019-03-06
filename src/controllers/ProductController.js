0bb6abb7463167b5cea75b79f725225f49ff5dc4
lucasGabrielDeAA <lg.technus@gmail.com>
2019-03-06 14:49:12 -0300

    saveProduct's functionality added to ProductController

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -3,9 +3,15 @@ const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 
 module.exports = {
-    async index(req, res) {
+    async all(req, res) {
         const products = await Product.find();
 
         return res.json(products);
+    },
+
+    async save(req, res) {
+        const product = await Product.create(req.body);
+
+        return res.json(product);
     },
 };
