48f2d7cd0fe8a54e97d4a7eea546eb0638843064
lucasGabrielDeAA <lg.technus@gmail.com>
2019-03-06 14:49:25 -0300

    Route to save products added

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const routes = express.Router();
 
 const ProductController = require('./controllers/ProductController');
 
-routes.get('/products', ProductController.index);
+routes.get('/products', ProductController.all);
+routes.post('/products', ProductController.save);
 
 module.exports = routes;
