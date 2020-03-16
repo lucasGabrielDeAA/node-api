7e0e27475d9e4013aa9c71b079bd98b14312fdaf
lucasGabrielDeAA <lg.technus@gmail.com>
2020-03-16 11:16:39 -0300

    Admin-bro added

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 const cors = require('cors');
 
+const DATABASE_URL = 'mongodb://localhost:27017/nodeapi';
+const PORT = 3001;
+
+const adminRouter = require('./admin.router');
+
 //Initializing app
 const app = express();
 app.use(express.json());
@@ -10,9 +15,12 @@ app.use(express.json());
 //Using cross-origin request security
 app.use(cors());
 
+//Adding admin router
+app.use('/admin', adminRouter);
+
 //Initializing DB
 mongoose.connect(
-    'mongodb://localhost:27017/nodeapi',
+    DATABASE_URL,
     { useNewUrlParser: true }
 );
 
@@ -22,4 +30,4 @@ requireDir('./src/models/');
 //Routes
 app.use('/api', require('./src/routes'));
 
-app.listen(3001);
+app.listen(PORT, () => console.log(`Admin console is running under localhost:${PORT}/admin`));
